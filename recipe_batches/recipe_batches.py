d27813ca2880be39bad44bf07199047d50cd71d0
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-08-14 16:05:45 -0700

    polya for knapsack, recipe test passes

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,31 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  #base case
+  #if no ingredients, retun 0
+  if len(ingredients) == 0:
+    return 0
+  #keep a count of how many batches we can make
+  count = 0
+  #check the number of ingredients
+  while len(ingredients) > 0:
+    #subtract the recipe amount from ingredients on hand. if not enough ingredients, we can't make a batch so return the count
+    for i in recipe.items():
+      if i in ingredients:
+        if ingredients[i] - recipe[i] < 0:
+          return count
+        #if we have enough ingredients, subtract the required recipe amount from ingredients on hand
+        else:
+          ingredients[i] = ingredients[i] - recipe[i]
+      #else we dont have enough ingredients, return 0
+      else:
+        return 0  
+    #increase the count
+    count += 1
+  #return the count
+  return count
+
+  
 
 
 if __name__ == '__main__':
