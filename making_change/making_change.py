a0375125e64f80d26a2cd923db1b9962a75572ed
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-08-15 15:39:01 -0700

    recursive making change, but hangs on large test

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,7 +3,21 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
+  #base case
+  #What should happen when the amount of cents given is 0?
+  #if amount is 0, there is only 1 way to denominate change
+  if amount == 0:
+    return 1
+  #What should happen when the amount of cents given is negative?
+  #if amount is less than 0, there is no way to denominate the change 
+  if amount < 0:
+    return 0
+  #What about when we've used up all of the available coin denominations?
+  #if denominations is 0, there is no way to denominate change
+  if len(denominations) == 0:
+    return 0
+  #changing out will be the sum of both counts for possible ways to make change
+  return making_change(amount - denominations[-1], denominations) + making_change(amount, denominations[:-1])
 
 
 if __name__ == "__main__":
